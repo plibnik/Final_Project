20f221e68d80a5becc46534183538e6271795ac7
plibnik <113441451+plibnik@users.noreply.github.com>
2023-02-15 09:23:43 +0200

    initial
    Population with non-VCS fodder from before

---
+++ payload/the_daemon/daemon.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+powerdb = mysql.connector.connect(
+  host="localhost",
+  user="daemon_user", # REPLACE this later with SCM-pulled credentials!
+  password="yourpassword", # REPLACE this too with SCM-pulled credentials!
+  database="ENERGY_MONITORING"
+)
+
+print(mydb) 
